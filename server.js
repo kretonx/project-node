30b7f26a4a75469201279e22bfca3d2942ddf369
unknown <otaviorufinno@gmail.com>
2026-01-15 06:54:12 -0300

    feat: create tickets in memory (POST) and list tickets (GET)

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const http = require('http');
+const tickets = [];
 const { json } = require('stream/consumers');
 
 const server = http.createServer((req, res)=>{
-  console.log(req.method, req.url);
+  console.log('METHOD:', req.method);
+  console.log('URL:', JSON.stringify(req.url));
+
+  let body = '';
+
+  req.on('data', chunk =>{
+    body += chunk;
+  });
+
+  req.on('end', () =>{
+    if(req.url === '/tickets'&& req.method === 'POST'){
+      const data = JSON.parse(body);
+
+      const ticket = {
+        id: tickets.length + 1,
+        title: data.title,
+        description: data.description,
+        status: 'open',
+        createdAt: new Date().toISOString()
+      };
+
+      tickets.push(ticket);
+
+      res.writeHead(201, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(ticket));
+      return;
+    }
+
+  
+
   if (req.url === 'health' && req.method === 'GET'){
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({status: 'ok'}));
@@ -19,7 +49,7 @@ const server = http.createServer((req, res)=>{
   res.writeHead(404, {'Content-Type': 'application/json'});
   res.end(JSON.stringify({error: 'rota nao encontrada'}));
 })
-
+});
 
 
 server.listen(3000, () => {
